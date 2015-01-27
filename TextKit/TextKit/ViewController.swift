d9f036bec90388885f28749223b79f4323c49b11
Emiaostein <cszwdy@gmail.com>
2015-01-27 14:23:07 +0800

    exclusionPaths

--- TextKit/TextKit/ViewController.swift
+++ TextKit/TextKit/ViewController.swift
@@ -8,29 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     @IBOutlet weak var textView: UITextView!
+    
+    let timeIndicatorView: TimeIndicatorView! = TimeIndicatorView(date: NSDate(timeIntervalSinceNow: 0))
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "preferredContentSizeDidChanged:", name: UIContentSizeCategoryDidChangeNotification, object: nil)
         
+        self.view.addSubview(timeIndicatorView)
+        
         self.updateFont()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        self.updateTimeIndicatorFrame()
+    }
+    
+}
+
+// MARK: Public Method
+extension ViewController {
+    
+    
+}
+
+// MARK: TimeIdicatorView
+extension ViewController {
+    
+    private func updateTimeIndicatorFrame() {
+        
+        timeIndicatorView.updateSize()
+        
+        timeIndicatorView.frame = CGRectOffset(timeIndicatorView.frame,
+            CGRectGetWidth(self.view.frame) - CGRectGetWidth(timeIndicatorView.frame),
+            0.0)
+        
+        let exclusionPath = timeIndicatorView.curvePathWithOrigin(timeIndicatorView.center)
+        textView.textContainer.exclusionPaths = [exclusionPath]
+        
     }
 }
 
 // MARK: textKit-Notifiction
 extension ViewController {
     
-    func preferredContentSizeDidChanged(notifiction: NSNotification) {
+    private func preferredContentSizeDidChanged(notifiction: NSNotification) {
         
         self.updateFont()
+        self.updateTimeIndicatorFrame()
         
     }
     
-    func updateFont() {
+    private func updateFont() {
         
         let font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
         let color = UIColor.blueColor()
@@ -42,6 +78,8 @@ extension ViewController {
         
         
         self.textView.attributedText = attrString
+        
+        self.updateTimeIndicatorFrame()
     }
     
 }
