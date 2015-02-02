72ded898cd45b20772eb35c5c8936ab16d392471
Emiaostein <cszwdy@gmail.com>
2015-02-02 14:56:48 +0800

    add "web" assets

---
+++ HTML&iOS/HTML&iOS/web/js/index.js
@@ -0,0 +1,16 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery||window.Zepto);
+ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+$(document).ready(function() {
+r = function() {
+if($(window).width() >= 960) {
+var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/eagle-2412.png' : 'images/eagle-1608.png') : 'images/eagle-804-2.png');
+var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ludlow-ventures-2412.png' : 'images/ludlow-ventures-1608.png') : 'images/ludlow-ventures-804-2.png');
+} else {
+var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/eagle-804.png' : 'images/eagle-536.png') : 'images/eagle-268.png');
+var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ludlow-ventures-804.png' : 'images/ludlow-ventures-536.png') : 'images/ludlow-ventures-268.png');
+}
+};
+$(window).resize(r);
+r();
+$('.gallery .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: true,infinite: false});
+});
